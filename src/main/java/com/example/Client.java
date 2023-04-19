456b07bcb9e40c5bdcba1a062eba8099d3f81520
Almer8 <artem.holubiev@nure.ua>
2023-04-19 18:45:42 +0300

    Small updtes

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +11,7 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class Client extends Application {
 
 
     @Override
